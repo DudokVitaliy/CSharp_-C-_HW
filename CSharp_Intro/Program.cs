b83577dfe6d6336d7501e164278e53526ae7ba0c
User <User@WIN-A52UJKULNOP>
2024-06-16 12:11:30 +0300

    Добавьте файлы проекта.

---
+++ CSharp_Intro/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace CSharp_Intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("\t\tN1");
+            Console.WriteLine("It's easy to win forgiveness for being wrong;\nbeing right is what gets you into real trouble.\nBjarne Stroustrup");
+            
+            Console.WriteLine("\t\tN2");
+            Console.WriteLine("Enter 5 numbers: ");
+            Console.Write("\t1-th: "); int num1 = int.Parse(Console.ReadLine());
+            Console.Write("\t2-th: "); int num2 = int.Parse(Console.ReadLine());
+            Console.Write("\t3-th: "); int num3 = int.Parse(Console.ReadLine());
+            Console.Write("\t4-th: "); int num4 = int.Parse(Console.ReadLine());
+            Console.Write("\t5-th: "); int num5 = int.Parse(Console.ReadLine());
+            int sum = num1 + num2 + num3 + num4 + num5;
+            int prod = num1 * num2 * num3 * num4 * num5;
+            int max = num1;
+            if (num2 > max)
+                max = num2;
+            if (num3 > max)
+                max = num3;
+            if (num4 > max)
+                max = num4;
+            if (num5 > max)
+                max = num5;
+            int min = num1;
+            if (num2 < min)
+                min = num2;
+            if (num3 < min)
+                min = num3;
+            if (num4 < min)
+                min = num4;
+            if (num5 < min)
+                min = num5;
+            Console.WriteLine("All numbers:" + num1 + "; " + num2 + "; " + num3 + "; " + num4 + "; " + num5);
+            Console.WriteLine("\tsum = " + sum);
+            Console.WriteLine("\tprod = " + prod);
+            Console.WriteLine("\tmax = " + max);
+            Console.WriteLine("\tmin = " + min);
+            
+            Console.WriteLine("\t\tN3");
+            Console.Write("\tEnter number: \t"); int num = int.Parse(Console.ReadLine());
+            int a = num / 100000;
+            int b = num / 10000 % 10;
+            int c = num / 1000 % 10;
+            int d = num / 100 % 10;
+            int e = num / 10 % 10;
+            int f = num % 10;
+            Console.WriteLine("\tReverse: \t" + f + e + d + c + b + a);
+            */
+            Console.WriteLine("\t\tN4");
+            Console.Write("\tEnter start: \t"); int range_1 = int.Parse(Console.ReadLine());
+            Console.Write("\tEnter end: \t"); int range_2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\tResult: ");
+            while (range_1 > range_2)
+            {
+                
+            }
+        }
+    }
+}
