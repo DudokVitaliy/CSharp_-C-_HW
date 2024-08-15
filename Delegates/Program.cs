c9d29f1607244252a225ccea2151af354426b441
Dudok_kh03@student.itstep.org <Dudok_kh03@student.itstep.org>
2024-08-15 23:04:13 +0300

    Delegates HW

---
+++ Delegates/Program.cs
@@ -0,0 +1,185 @@
+﻿using System.Dynamic;
+using System.Linq.Expressions;
+
+namespace Delegates
+{
+    //class MyDelegate : Delegate  
+    //{
+
+    //}
+    public delegate int IntDelegate(double value);
+
+    public delegate void SetStringDeleg(string value);
+    public delegate double DoubleDelegate();
+    public delegate void VoidDelegate();
+    class SuperClass
+    {
+        public void Print(string str)
+        {
+            Console.WriteLine(str + "!!!");
+        }
+        public static double GetKoef()
+        {
+            double res = new Random().NextDouble();
+            return res;
+        }
+        public static double GetNumber()
+        {
+            return new Random().Next();
+        }
+        public void DoWork()
+        {
+            Console.WriteLine("I do some work!");
+        }
+        public void Test()
+        {
+            Console.WriteLine("I testing.....");
+        }
+    }
+    public delegate double CalculatorDelegate(double x, double y);
+    class Calculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public double Sub(double x, double y)
+        {
+            return x - y;
+        }
+        public double Multy(double x, double y)
+        {
+            return x * y;
+        }
+        public double Div(double x, double y)
+        {
+            if (y != 0)
+                return x / y;
+            else
+                throw new DivideByZeroException();
+        }
+    }
+    public delegate int ChangeDelegate(int value);
+   
+    internal class Program
+    {
+        public static void ChangeElements(int[] arr, ChangeDelegate change)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = change(arr[i]);
+            }
+        }
+        static int Sqr(int n)
+        {
+            return n * n;
+        }
+        static int Increment(int n)
+        {
+            return ++n;
+        }
+        static int Decrement(int n)
+        {
+            return --n;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            foreach (int i in arr)  Console.Write(i + "; "); Console.WriteLine();
+            ChangeElements(arr, Sqr);
+            foreach (int i in arr)  Console.Write(i + "; "); Console.WriteLine();
+            ChangeElements(arr, Increment);
+            foreach (int i in arr) Console.Write(i + "; "); Console.WriteLine();
+            ChangeElements(arr, Decrement);
+            foreach (int i in arr) Console.Write(i + "; "); Console.WriteLine();
+
+            ChangeElements(arr, delegate(int v) { return v + 10; });
+            foreach (int i in arr) Console.Write(i + "; "); Console.WriteLine();
+            //LambdaExpression 
+            ChangeElements(arr, v => --v);
+            foreach (int i in arr) Console.Write(i + "; "); Console.WriteLine();
+            /*
+            Calculator calculator = new Calculator();
+            CalculatorDelegate calcDelegate = null;
+            calcDelegate += calculator.Add;
+            calcDelegate += calculator.Div;
+            calcDelegate += calculator.Sub;
+            calcDelegate += calculator.Multy;
+            foreach (var item in calcDelegate.GetInvocationList())
+            {
+                Console.WriteLine($"Res = {(item as CalculatorDelegate)!.Invoke(10, 100)}");
+            }
+            */
+            /*
+            Calculator calculator = new Calculator();
+            double x, y;
+            int key;
+            do
+            {
+                CalculatorDelegate calcDelegate = null;
+                Console.WriteLine("Enter first number ");
+                x = double.Parse(Console.ReadLine()!);
+                Console.WriteLine("Enter second number ");
+                y = double.Parse(Console.ReadLine()!);
+                Console.WriteLine("Add  - 1 ");
+                Console.WriteLine("Sub  - 2 ");
+                Console.WriteLine("Mult  - 3 ");
+                Console.WriteLine("Divide  - 4 ");
+                Console.WriteLine("Exit  - 0 ");
+                key = int.Parse(Console.ReadLine()!);
+                switch (key)
+                {
+                    case 1:
+                        calcDelegate = new CalculatorDelegate(calculator.Add);
+                        break;
+                    case 2:
+                        calcDelegate = new CalculatorDelegate(calculator.Sub);
+                        break;
+                    case 3:
+                        calcDelegate = calculator.Multy;
+                        break;
+                    case 4:
+                        calcDelegate = calculator.Div;
+                        break;
+                    case 0:
+                        Console.WriteLine("Good  Buy");
+                        break;
+                    default:
+                        Console.WriteLine("Error choice......");
+                        break;
+                }
+
+                Console.WriteLine("Res = " + calcDelegate?.Invoke(x, y));
+            } while (key != 0);
+
+
+            //SuperClass superClass = new SuperClass();
+            //SuperClass.GetKoef();
+            //DoubleDelegate method = new DoubleDelegate(SuperClass.GetKoef);
+            //Console.WriteLine(method?.Invoke());
+            //DoubleDelegate[] arr = new DoubleDelegate[]
+            //{
+            //     SuperClass.GetKoef,
+            //     SuperClass.GetNumber
+            //};
+            //Console.WriteLine(arr[0].Invoke());
+            //Console.WriteLine(arr[1]());
+            //SetStringDeleg stringDeleg = new SetStringDeleg(superClass.Print);
+            //VoidDelegate voidDeleg = new VoidDelegate(superClass.DoWork);
+            //stringDeleg.Invoke("Hellllllo");
+            //voidDeleg();
+            ////Delegate.Combine(method, SuperClass.GetNumber);
+            //method += SuperClass.GetNumber;
+            //method += SuperClass.GetNumber;
+            //method += SuperClass.GetKoef;
+
+            //foreach (var item in method.GetInvocationList())
+            //{
+            //    Console.WriteLine((item as DoubleDelegate)!.Invoke());
+            //}
+            */
+        }
+    }
+
+}
+
---
+++ Delegates_HW/Program.cs
@@ -0,0 +1,222 @@
+﻿namespace Delegates_HW
+{
+    using System.Reflection.Metadata.Ecma335;
+    using static System.Console;
+    public delegate int ResDeleg();
+    public delegate void ChangeDeleg();
+    class MyArray
+    {
+        int[] arr;
+        public MyArray(int count)
+        {
+            arr = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                arr[i] = new Random().Next(-10, 10);
+            }
+        }
+        public void Print()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Write(arr[i] + "; ");
+            }
+            WriteLine();
+        }
+        //////////////////// 
+        public int CountMinus()
+        {
+            int count = 0;
+            for(int i = 0;i < arr.Length;i++)
+            {
+                if (arr[i] < 0)
+                    count++;
+            }
+            return count;
+        }
+        public int Summa()
+        {
+            int summa = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                summa += arr[i];
+            }
+            return summa;
+        }
+        public int CountSimple()
+        {
+            int count = 0;
+            bool simple = true;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 1)
+                {
+                    for (int j = 2; j <= arr[i] / 2; j++)
+                    {
+                        if (arr[i] % j == 0)
+                        {
+                            simple = false;
+                            break;
+                        }
+                    }
+                }
+                else if (arr[i] < -1)
+                {
+                    for (int j = -2; j >= arr[i] / 2; j--)
+                    {
+                        if (arr[i] % j == 0)
+                        {
+                            simple = false;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    simple = false;
+                }
+                if (simple)
+                {
+                    count++;
+                    //WriteLine(arr[i]);
+                }
+                simple = true;
+            }
+
+            return count;
+        }
+        ///////////////////////////////////
+        public void MinusToZero()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                    arr[i] = 0;
+            }
+        }
+        public void Sort()
+        {
+            Array.Sort(arr);
+        }
+        public void Replace()
+        {
+            int[] new_arr = new int[arr.Length];
+            int j = 0;
+            for(int i = 0;i < arr.Length;i++)
+            {
+                if (arr[i] % 2 != 0)
+                {
+                    new_arr[j]  = arr[i];
+                    j++;
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    new_arr[j] = arr[i];
+                    j++;
+                }
+            }
+            arr = new_arr;
+        }
+    }
+    internal class Program
+    {
+        static public void NextMenu(int i)
+        {
+            int choise;
+            if (i == 1)
+            {
+                WriteLine("\tSelect Operation");
+                WriteLine("1 - count of negative elements");
+                WriteLine("2 - sum of all elements");
+                WriteLine("3 - the number of prime numbers");
+                Write("Choise -> "); choise = int.Parse(ReadLine());
+                Calculation(choise);
+            }
+            else if (i == 2)
+            {
+                WriteLine("\tSelect Operation");
+                WriteLine("1 - change all negative elements to 0");
+                WriteLine("2 - sort array");
+                WriteLine("3 - move all even elements to the beginning");
+                Write("Choise -> "); choise = int.Parse(ReadLine());
+                Change(choise);
+            }
+            else
+            {
+                WriteLine("Wrong Select!"); 
+            }
+        }
+        static public void Calculation(int i)
+        {
+            ResDeleg res;
+            MyArray array = new MyArray(10);
+            WriteLine("Your random array: ");
+            array.Print();
+            if (i == 1)
+            {
+                res = new ResDeleg(array.CountMinus);
+                WriteLine($"Result: {res.Invoke()}");
+            }
+            else if (i == 2)
+            {
+                res = new ResDeleg(array.Summa);
+                WriteLine($"Result: {res.Invoke()}");
+            }
+            else if (i == 3)
+            {
+                res = new ResDeleg(array.CountSimple);
+                WriteLine($"Result: {res.Invoke()}");
+            }
+            else
+            {
+                WriteLine("Wrong Select!");
+            }
+        }
+        static public void Change(int i)
+        {
+            MyArray array = new MyArray(10);
+            WriteLine("Your random array: ");
+            array.Print();
+            if (i == 1)
+            {
+                ChangeDeleg res = new ChangeDeleg(array.MinusToZero);
+                res.Invoke();
+            }
+            else if (i == 2)
+            {
+                ChangeDeleg res = new ChangeDeleg(array.Sort);
+                res.Invoke();
+            }
+            else if (i == 3)
+            {
+                ChangeDeleg res = new ChangeDeleg(array.Replace);
+                res.Invoke();
+            }
+            else
+            {
+                WriteLine("Wrong Select!");
+            }
+            WriteLine("Result:");
+            array.Print();
+
+        }
+        static void Main(string[] args)
+        {
+            MyArray array = new MyArray(10);
+            int choise = 1;
+            while (choise != 0) 
+                {
+                    WriteLine("\tMENU");
+                    WriteLine("1 - Calculation of the value");
+                    WriteLine("2 - Array change");
+                    WriteLine("0 - Exit");
+                    Write("Choise -> "); choise = int.Parse(ReadLine());
+                    NextMenu(choise);
+                }
+            WriteLine("Goodbye!");
+        }
+    }
+}
